a25e35c9ba61e49130ed876420fad3a12e62ac69
Pablo Perez <pablo.perez.rodriguez@udc.es>
2022-08-04 18:13:26 +0200

    Indexing seems fully functional

--- event_reader/IndexedEventReader.h
+++ event_reader/IndexedEventReader.h
@@ -12,41 +12,14 @@ class IndexedEventReader : public DiskEventReader {
 public:
     explicit IndexedEventReader(int _fixedPayloadSize) : DiskEventReader(_fixedPayloadSize) {}
 
+    // Find the closest event to start in the index and start the sequential scan from there until end
     std::list<std::pair<EID, const char *>> *
     readEventsInRange(const CID &cid, std::time_t start, std::time_t end) override {
-        // Very naive implementation: start reading from beginning until EID >= start is found
-        // Read from that point until EID > end is found
-        // Alternative: perform binary search to find an event in the range, and start from there
 
-        std::ifstream file = openReadFile(cid + LOG_EXTENSION);
-        std::streampos fileSize = file.tellg();
-        auto *events = new std::list<std::pair<EID, const char *>>;
+        // We will start reading either from the beginning or from the closest index entry
+        long i = start == VOID_TIMESTAMP ? 0 : MemoryIndex::getClosestValue(start);
 
-        int i = 0;
-        // Size = payload + 10 (EID) + comma + semicolon
-        int readSize = fixedPayloadSize + 10 + 1 + 1;
-        while (i + readSize <= fileSize) {
-            char *id = new char[10];
-            char *data = new char[fixedPayloadSize + 1];
-
-            file.seekg(i);
-            file.get(id, 11);
-            auto eid = (std::time_t) strtol(id, nullptr, 10);
-
-            if ((start == VOID_TIMESTAMP || eid >= start) && (end == VOID_TIMESTAMP || eid <= end)) {
-                file.seekg(i + 10 + 1);
-                file.get(data, fixedPayloadSize + 1);
-                events->push_back(std::pair(eid, data));
-            }
-
-            if (end != VOID_TIMESTAMP && eid > end) {
-                break;
-            }
-
-            i += readSize;
-        }
-
-        return events;
+        return DiskEventReader::readEventsInRange(cid + LOG_EXTENSION, i, start, end);
     }
 };
 
